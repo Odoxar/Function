dbe3c52f26034ddfffd87355a888a8d5bbefaff6
Denys Siumak <odoxar@gmail.com>
2022-12-25 17:33:05 +0200

    remove input object and result comments

--- Exercises/4-methods.js
+++ Exercises/4-methods.js
@@ -2,20 +2,6 @@
 
 // Introspect all properties of iface object and
 // extract function names and number of arguments
-// const obj = {
-//   m1: (x,) => [x],
-//   // m2: function (x, y) {
-//   //   return [x, y];
-//   // },
-//   m3(x, y, z) {
-//     return [x, y, z];
-//   },
-// };
-// will return: [
-//   ['m1', 1],
-//   ['m2', 2],
-//   ['m3', 3]
-// ]
 
 const methods = (iface) => {
   const res = [];
