84abc42632a64981fa8717a7fb8804f15280665b
Denys Siumak <odoxar@gmail.com>
2022-12-25 17:29:51 +0200

    Lab: Function (completed)

--- Exercises/2-key.js
+++ Exercises/2-key.js
@@ -1,9 +1,16 @@
 'use strict';
 
+// Generate string of random characters
+// Use Math.random() and Math.floor()
+// See documentation at MDN
 const generateKey = (length, possible) => {
-  // Generate string of random characters
-  // Use Math.random() and Math.floor()
-  // See documentation at MDN
+  let res = '';
+  const len = possible.length;
+  for (let i = 0; i < length; i++) {
+    const index = Math.floor(Math.random() * len);
+    res += possible[index];
+  }
+  return res;
 };
 
 module.exports = { generateKey };
--- Exercises/4-methods.js
+++ Exercises/4-methods.js
@@ -1,21 +1,31 @@
 'use strict';
 
-const methods = iface => {
-  // Introspect all properties of iface object and
-  // extract function names and number of arguments
-  // For example: {
-  //   m1: x => [x],
-  //   m2: function (x, y) {
-  //     return [x, y];
-  //   },
-  //   m3(x, y, z) {
-  //     return [x, y, z];
-  //   }
-  // will return: [
-  //   ['m1', 1],
-  //   ['m2', 2],
-  //   ['m3', 3]
-  // ]
+// Introspect all properties of iface object and
+// extract function names and number of arguments
+// const obj = {
+//   m1: (x,) => [x],
+//   // m2: function (x, y) {
+//   //   return [x, y];
+//   // },
+//   m3(x, y, z) {
+//     return [x, y, z];
+//   },
+// };
+// will return: [
+//   ['m1', 1],
+//   ['m2', 2],
+//   ['m3', 3]
+// ]
+
+const methods = (iface) => {
+  const res = [];
+  for (const key in iface) {
+    const fn = iface[key];
+    if (typeof fn === 'function') {
+      res.push([key, fn.length]);
+    }
+  }
+  return res;
 };
 
 module.exports = { methods };
